cd02969ddbc335246c5ed9232c113927456f423f
Ian Wells <iawells@cisco.com>
2013-01-15 08:39:41 -0800

    Bring dkms rebuild out into its own task, as it doesn't run reliably during the install step; save its output; change deps around; sort package ensure for kernel headers

--- manifests/ovs.pp
+++ manifests/ovs.pp
@@ -10,10 +10,18 @@ class vswitch::ovs(
         default: {
              # OVS doesn't build unless the kernel headers are present.
              $ovs_module_req_pkg = "linux-headers-$::kernelrelease"
+
+             exec { 'rebuild-ovsmod':
+               command => "/usr/sbin/dpkg-reconfigure openvswitch-datapath-dkms > /tmp/reconf-log",
+               creates => "/lib/modules/$::kernelrelease/updates/dkms/openvswitch_mod.ko",
+               require => [Package['openvswitch-datapath-dkms', $ovs_module_req_pkg]],
+               before  => Package['openvswitch-switch'],
+             }
+
           }
       }
       if ! defined(Package[$ovs_module_req_pkg]) {
-        package{ $ovs_module_req_pkg: ensure => present }
+        package{ $ovs_module_req_pkg: ensure => $package_ensure }
       }
     
       package {["openvswitch-common",
@@ -28,8 +36,12 @@ class vswitch::ovs(
   service {"openvswitch-switch":
     ensure      => true,
     enable      => true,
-    hasstatus   => false, # the supplied command returns true even if it's not running
+    hasstatus   => $::operatingsystem ? {
+          'Ubuntu' => true,
+          default  => false, # the supplied command returns true even if it's not running
+    }
     # Not perfect - should spot if either service is not running - but it'll do
+    # will only apply if $hasstatus is false
     status      => "/etc/init.d/openvswitch-switch status | fgrep 'is running'",
   }
 
